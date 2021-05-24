efae99f1fe22623a8d3af8cb5a818e1313606999
C1300284 Thiago Soares Figueiredo <c1300284@interno.bb.com.br>
2021-05-23 22:18:38 -0300

    Etilizacao inicial com styled-components

---
+++ dtmoney/src/styles/global.ts
@@ -0,0 +1,56 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --backgorund: #f0f0c1;
+        --red: #e52e4d;
+        --blue: #5429cc;
+        --blue-light: #6933ff;
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+        --shape: #ffffff;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    /* default: 16px
+    * 1rem = 1 default
+    * 1 unidade de rem :: equivale 6,25 unidades em %
+    */
+    html {
+        @media (max-width: 1080px) {
+            font-size: 93.75%;  // 15px = 16 (default) * 0.9375
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.50%;  // 14px = 16 (default) * 0.8750
+        }
+    }
+
+    body {
+        background-color: var(---backgorund);
+        --webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`;
